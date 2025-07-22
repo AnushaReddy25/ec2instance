db5555690f8c667cb70bedd1f70abc5f7d841d9e
Anusha Reddy <123926678+AnushaReddy25@users.noreply.github.com>
2025-07-22 15:44:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
-  default = "us-east-1"
+  default = "eu-north-1"
 }
 
 variable "ami_id" {
-  default = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 for us-east-1
+  default = "ami-013ed6046abe69f0f" # Amazon Linux 2 for us-east-1
 }
 
 variable "instance_type" {
